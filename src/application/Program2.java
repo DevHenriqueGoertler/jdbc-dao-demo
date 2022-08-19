535f492156ff28880f5e8c3b3795d0dfaac6f816
Henrique Goertler <dev.henriquegoertler@gmail.com>
2022-08-19 12:22:17 -0300

    delete and update implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
@@ -9,6 +10,8 @@ import model.entities.Department;
 public class Program2 {
 
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
 		
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 		
@@ -22,10 +25,25 @@ public class Program2 {
 			System.out.println(d);
 		}
 		
-		System.out.println("\n=== TEST3: department Insert =====");
-		Department newDep = new Department(null, "Vehicles");
+		System.out.println("\n=== TEST3: department insert =====");
+		Department newDep = new Department(null, "Hardware");
 		departmentDao.insert(newDep);
-		System.out.println("Inserted! New Id = " + newDep.getId()); 
+		System.out.println("Inserted! New id = " + newDep.getId());
+	
+	
+		System.out.println("\n=== TEST4: department update =====");
+		dep = departmentDao.findById(1);
+		dep.setName("Computer");
+		departmentDao.update(dep);
+		System.out.println("Update completed");
+		
+		System.out.println("\n=== TEST5: department delete =====");
+		System.out.print("Enter id for delete test: ");
+		int id = sc.nextInt();
+		departmentDao.deleteById(id);
+		System.out.println("Delete completed");
+		
+		sc.close();
 	}
 
 }
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -53,13 +53,41 @@ public class DepartmentDaoJDBC implements DepartmentDao{
 
 	@Override
 	public void update(Department obj) {
-		// TODO Auto-generated method stub
-		
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement(
+					"UPDATE department "
+					+ "SET Name = ? "
+					+ "WHERE Id = ?");
+			
+			st.setString(1, obj.getName());
+			st.setInt(2, obj.getId());
+			
+			st.executeUpdate();
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
 	public void deleteById(Integer id) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement(
+					"DELETE FROM department "
+					+ "WHERE Id = ?");
+			
+			st.setInt(1, id);
+					
+			st.executeUpdate();
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+		}
+		
 		
 	}
 
